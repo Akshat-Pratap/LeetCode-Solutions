20bf46113790b666663ff603742879464b805b3e
Akshat Pratap <akshatpratap2020@gmail.com>
2026-01-13 21:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.62%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x<2)
+            return x;
+        double y=x;
+        double z= (y+(x/y))/2;
+        while ((y-z)>0.1)
+        {
+            y=z;
+            z= (y+(x/y))/2;
+        }
+        return z;
+    }
+};

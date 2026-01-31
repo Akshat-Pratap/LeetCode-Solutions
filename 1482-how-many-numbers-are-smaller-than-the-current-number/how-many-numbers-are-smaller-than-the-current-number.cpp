a64e55e5fa9013d3652a90ae7f56de59d4f46979
Akshat Pratap <akshatpratap2020@gmail.com>
2026-01-31 22:54:25 +0530

    Time: 7 ms (49.29%) | Memory: 14.1 MB (94.77%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -4,10 +4,13 @@ public:
         vector<int> res;
         for (int i = 0; i < nums.size(); i++) {
             int count = 0;
-            for (int j = 0; j < nums.size(); j++)
-                {if (nums[i] > nums[j]) 
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[i] > nums[j]) {
                     count++;
                 }
+            }
             res.push_back(count);
         }
-        return res;}};
+        return res;
+    }
+};

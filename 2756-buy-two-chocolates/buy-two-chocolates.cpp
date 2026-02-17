cc2bdb9629df47dcc25c20de247536968d6d42e9
Akshat Pratap <akshatpratap2020@gmail.com>
2026-02-17 19:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (93.57%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int res=money;
+        res=res-(prices[0]+prices[1]);
+        if(res<0)
+            return money;
+        else
+            return res;
+    }
+};

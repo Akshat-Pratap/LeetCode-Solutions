3eb8604ea504383a9edc45ba6c5ad0b703748fc3
Akshat Pratap <akshatpratap2020@gmail.com>
2026-05-17 21:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.64%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==1) return true;
+        if(n<=0||n%2!=0)
+            return false;
+        while(n>2)
+        {
+            if(n%2!=0)
+                return false;
+            n/=2;
+        }
+        return true;
+    }
+};

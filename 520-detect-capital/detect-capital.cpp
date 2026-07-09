3d2679678cc7ff63b3b0b1821defaa6a24f03b1e
Akshat Pratap <akshatpratap2020@gmail.com>
2026-07-09 23:56:25 +0530

    Time: 3 ms (3.73%) | Memory: 8.2 MB (50.77%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int upperCount=0;
+        for(char c:word)
+        {
+            if(isupper(c))
+                upperCount++;
+        }
+        if(upperCount==word.length() || upperCount==0)
+            return true;
+        if(upperCount==1 && isupper(word[0]))
+            return true;
+        return false;
+    }
+};

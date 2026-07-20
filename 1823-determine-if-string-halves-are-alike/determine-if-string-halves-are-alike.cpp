2e59257c0fc008a69f48cc9efbe4954b16db624b
Akshat Pratap <akshatpratap2020@gmail.com>
2026-07-20 22:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (75.70%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n=s.size();
+        int count = 0;
+        string vowels="aeiouAEIOU";
+        for(int i=0;i<n/2;i++)
+        {
+            if(vowels.find(s[i])!= string::npos)
+                count++;
+            if(vowels.find(s[n/2 + i])!= string::npos)
+                count--;
+        }
+        return count==0;
+    }
+};
